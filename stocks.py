9ff484abaa5adae61622f74c38effd265deff649
Hareesh Ganesan <hareesh@towerviewhealth.com>
2018-07-07 13:03:44 -0400

    adding intraday support for mutual funds

--- stocks.py
+++ stocks.py
@@ -23,18 +23,43 @@ def parse_security(row):
 		'commission' : float(row[5])
 	}
 
+def determine_cost_basis(position):
+	cost_basis = 0
+	num_shares = 0
+	for order in position['orders']:
+		if order['type'] == 'buy':
+			cost_basis += (order['num_shares'] * order['share_price']) + order['commission']
+			num_shares += order['num_shares']
+		if order['type'] == 'sell':
+			num_shares -= order['num_shares']
+	
+	return cost_basis, num_shares
+
+def determine_market_value(portfolio):
+	# note: portfolio.keys() is a list of all symbols in the portfolio
+	# make the API call
+	symbols = portfolio.keys()
+	market_data = api_client.batch_quote(symbols)
+
+	for symbol in symbols:
+		if(symbol in market_data):
+			price = market_data[symbol]['price']
+		else:
+			price = api_client.get_most_recent_daily_close(symbol)
+			
+		if price:
+			summary = portfolio[symbol]['summary']
+			summary['market_value'] = price * summary['num_shares']
+
+def calculate_gain_loss(summary):
+	if(summary['market_value']):
+		summary['gain_loss'] = summary['market_value'] - summary['cost_basis']
+	
 def summarize_positions(portfolio):
 	# iterate over the symbols in the portfolio to calculate the portfolio summary
 	for symbol in portfolio:	
 		# determine cost basis / number of shares
-		cost_basis = 0
-		num_shares = 0
-		for order in portfolio[symbol]['orders']:
-			if order['type'] == 'buy':
-				cost_basis += (order['num_shares'] * order['share_price']) + order['commission']
-				num_shares += order['num_shares']
-			if order['type'] == 'sell':
-				num_shares -= order['num_shares']
+		cost_basis, num_shares = determine_cost_basis(portfolio[symbol])
 		# TODO: handle closed positions 
 		
 		portfolio[symbol]['summary']['cost_basis'] = cost_basis	
@@ -42,27 +67,18 @@ def summarize_positions(portfolio):
 
 	# for market value we could do this in the above loop, but would have to iterate through the 
 	# stock quote market_data for every security, so I think this is more efficient
-	# note: portfolio.keys() is a list of all symbols in the portfolio
-
-	# make the API call
-	market_data = api_client.batch_quote(portfolio.keys())
-
-	for symbol in market_data:
-		summary = portfolio[symbol]['summary']
-		price = float(market_data[symbol]['price'])
-		summary['market_value'] = price * summary['num_shares']
+	determine_market_value(portfolio)
 
 	# calculate gain / loss? this might be excessive....
 	total_gain_loss = 0
 	for symbol in portfolio:
 		summary = portfolio[symbol]['summary']
-
-		if(summary['market_value']):
-			summary['gain_loss'] = summary['market_value'] - summary['cost_basis']
+		
+		calculate_gain_loss(summary)
 		
 		# let's look at all open positions
-		if summary['num_shares'] > 0:
-			total_gain_loss += (summary['gain_loss'] or 0)
+		if summary['num_shares'] > 0 and summary['gain_loss']:
+			total_gain_loss += summary['gain_loss']
 
 	## TODO: add the portfolio summary to the bottom of the table
 	print('total gain / loss - ',round(total_gain_loss,2))
@@ -84,7 +100,7 @@ def create_portfolio():
 			security = parse_security(row)
 			symbol = row[2]
 
-			# if we don't have the symbol, add to the portfolio
+			# if we don't have the symbol, add entry to the portfolio
 			if symbol not in portfolio:
 				portfolio[symbol] = {
 					'summary' : {
@@ -103,7 +119,3 @@ def create_portfolio():
 	summarize_positions(portfolio)
 
 	return portfolio
-
-
-
-
